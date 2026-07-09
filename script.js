aaf0ce5291f3880c88072e5d75dd83540c43a72d
edenleroy59450-tech <eden.leroy59450@gmail.com>
2026-07-09 21:02:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const supabaseUrl = "https://dolhojxaiicjlsocthxl.supabase.co/rest/v1/";
+const supabaseUrl = "https://dolhojxaiicjlsocthxl.supabase.co";
 const supabaseKey = "sb_publishable_937F1rUifcDcxJEvD3Drxg_Qp9vDdSl";
 
 const supabaseClient = supabase.createClient(
     supabaseUrl,
     supabaseKey
 );
-
 
 const uploadBtn = document.getElementById("uploadBtn");
 const fileInput = document.getElementById("file");
@@ -16,23 +15,15 @@ uploadBtn.addEventListener("click", async () => {
 
     const file = fileInput.files[0];
 
-
     if (!file) {
-        status.innerHTML = "❌ Choisis un fichier avant d'envoyer";
+        status.innerHTML = "❌ Choisis un fichier";
         return;
     }
 
-
-    status.innerHTML = "⏳ Upload en cours...";
+    status.innerHTML = "⏳ Envoi...";
 
 
     const fileName = Date.now() + ".png";
-
-const { data, error } = await supabaseClient
-    .storage
-    .from("files")
-    .upload(fileName, file);
-        .replace(/[^a-zA-Z0-9.]/g, "-");
 
 
     const { data, error } = await supabaseClient
@@ -41,17 +32,13 @@ const { data, error } = await supabaseClient
         .upload(fileName, file);
 
 
-
     if (error) {
-
         console.error(error);
-        status.innerHTML = "❌ Erreur : " + error.message;
-
-    } else {
-
+        status.innerHTML = "❌ " + error.message;
+    } 
+    else {
         console.log(data);
-        status.innerHTML = "✅ Fichier envoyé avec succès !";
-
+        status.innerHTML = "✅ Envoyé !";
     }
 
 });
